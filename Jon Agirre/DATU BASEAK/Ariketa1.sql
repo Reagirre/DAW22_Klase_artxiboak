fdfd348c3af51debfbafde96a0e88407a0b8f059
Reagirre <118358411+Reagirre@users.noreply.github.com>
2022-12-16 12:38:25 +0100

    Cambio

--- Jon Agirre/DATU BASEAK/Ariketa1.sql
+++ Jon Agirre/DATU BASEAK/Ariketa1.sql
@@ -1,15 +1,3 @@
-CREATE TABLE Artikuluak (
-    ARTIKULUA VARCHAR(20) NOT NULL,
-    HOR_KODEA NUMERIC(3) NOT NULL,
-    PISUA NUMERIC(3) NOT NULL,
-    KATEGORIA VARCHAR(10) NOT NULL,
-    SALMENTA_PREZIOA NUMERIC(4),
-    SALMENTA_KOSTUA NUMERIC(4),
-    STOCK NUMERIC(5),
-    PRIMARY KEY (ARTIKULUA, HOR_KODEA, PISUA, KATEGORIA),
-    foreign key (HOR_KODEA) references hornitzaileak(HOR_KODEA),
-    check (salmenta_prezioa > 0), 
-    check ( salmenta_kostua > 0),
-    check (stock > 0),
-    check (kategoria = 'Lehen' or 'Bigarren' or 'Hirugarren')
-);
+select *
+from information_schema.table_constraints
+where constraint_schema = 'Adibidea'
