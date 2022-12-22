0033b58c1593b586d04d2fc6ada391806a81e52f
Reagirre <118358411+Reagirre@users.noreply.github.com>
2022-12-22 10:36:38 +0100

    Cambio

--- Jon Agirre/PROGRAMAZIOA/Erronka_2/Java/src/Kotxea_Gehitu.java
+++ Jon Agirre/PROGRAMAZIOA/Erronka_2/Java/src/Kotxea_Gehitu.java
@@ -2,14 +2,15 @@ import java.util.*;
 import java.io.*;
 public class Kotxea_Gehitu
 {
+    private static String kotxeak = "lib/Kotxeak.csv";
+    private static Kotxea kotxea1 = new Kotxea();
     public static void gehitu() 
     {
         Scanner sarrera = new Scanner(System.in);
 
         String aukera;
-        String kotxeak = "lib/Kotxeak.csv";
-        String sardena;
-        Kotxea kotxea = new Kotxea();
+        
+        int probatu;
 
         System.out.print("\n\nKotxe berri bat sartzeko atalera sartu zara.\n\n");
         System.out.print("Aurrera jarraitu nahi? (y/n)");
@@ -22,9 +23,16 @@ public class Kotxea_Gehitu
             {
                 BufferedWriter writer = new BufferedWriter(new FileWriter(kotxeak, true));
                 System.out.print("Sartu kodea: ");
-                sardena = Integer.toString(sarrera.nextInt());
-                
-                writer.write(sardena);
+                probatu = sarrera.nextInt();
+                while(!konprobatu(probatu))
+                {
+                    System.out.print("Sartu kodea berriro: ");
+                    probatu = sarrera.nextInt();
+                }
+                kotxea1.setId(probatu);
+
+            
+                writer.write(kotxea1.idatzi());
                 writer.close();
             } 
             catch (IOException e)
@@ -37,5 +45,28 @@ public class Kotxea_Gehitu
         
 
         sarrera.close();
+    }
+    private static boolean konprobatu(int x) 
+    {
+        boolean result = false;
+        int y;
+        try {
+            File kotxeakIra = new File(kotxeak);
+            Scanner reader = new Scanner(kotxeakIra);
+            String strin;
+            while(reader.hasNextLine())
+            {
+                strin = reader.nextLine();
+                int index = strin.indexOf(",");
+                y = Integer.parseInt(strin.substring(0, index));
+                if(x == y)
+                    result = true;
+            }
+            reader.close();
+        } catch (Exception e) {
+            // TODO: handle exception
+        }
+        
+        return result;
     }
 }
