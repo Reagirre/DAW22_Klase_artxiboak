1d3077e6bc80ea9a5d01d5d25c91869c8b9d41fe
Reagirre <118358411+Reagirre@users.noreply.github.com>
2022-12-22 14:19:23 +0100

    Cambio

--- Jon Agirre/PROGRAMAZIOA/Erronka_2/Java/src/Kotxea_Aldatu.java
+++ Jon Agirre/PROGRAMAZIOA/Erronka_2/Java/src/Kotxea_Aldatu.java
@@ -10,7 +10,6 @@ public class Kotxea_Aldatu
         String aukera;
         
         int probatu;
-        int probatu2;
 
         System.out.print("\n\nKotxe bat aldatzeko atalera sartu zara.\n\n");
         System.out.print("Aurrera jarraitu nahi? (y/n): ");
@@ -18,51 +17,22 @@ public class Kotxea_Aldatu
 
         if(aukera.equals("y"))
         {
-            System.out.print("\n\nOrain kotxearen datuak sartu mesedez\n\n");
+            System.out.print("\n\nIdentifikatzeko ze kotxe aldatu nahi duzun, kodea adierazi behar duzu. Kontuz honekin!\n\n");
             try 
             {
                 BufferedWriter writer = new BufferedWriter(new FileWriter(kotxeak, true));
+                File kotxeakIra = new File(kotxeak);
+                Scanner reader = new Scanner(kotxeakIra);
+                String strin;
+                strin = reader.nextLine();
+                int index = strin.indexOf(",");
                 System.out.print("Sartu kodea: ");
                 probatu = sarrera.nextInt();
-                while(Kotxea.konprobatu(probatu, 0) != false)
+                if(Kotxea.konprobatu(probatu, 0))
                 {
-                    System.out.print("Sartu kodea berriro: ");
-                    probatu = sarrera.nextInt();
+                    System.out.printf("Kodea [%s]", reader.nextLine());
                 }
-                kotxea1.setId(probatu);
 
-                System.out.print("Sartu produktuaren izena: ");
-                kotxea1.setIzena(sarrera.next());
-
-                System.out.print("Sartu produktuaren fabrikatzailea: ");
-                kotxea1.setFabrikatzailea(sarrera.next());
-
-                System.out.print("Sartu produktuaren data: ");
-                kotxea1.setProdukzio_data(sarrera.next());
-
-                System.out.print("Sartu produktuaren prezioa: ");
-                kotxea1.setPrezioa(sarrera.nextInt());
-
-                System.out.print("Sartu kotxearen id-a: ");
-                probatu2 = sarrera.nextInt();
-                while(Kotxea.konprobatu(probatu2, 4) != false)
-                {
-                    System.out.print("Sartu kodea berriro: ");
-                    probatu2 = sarrera.nextInt();
-                }
-                kotxea1.setId_kotxea(probatu2);
-
-                System.out.print("Sartu kotxearen modeloa: ");
-                kotxea1.setModeloa(sarrera.next());
-
-                System.out.print("Sartu kotxearen kolorea: ");
-                kotxea1.setKolorea(sarrera.next());
-
-                System.out.print("Sartu kotxearen tipoa: ");
-                kotxea1.setTipoa(sarrera.next());
-
-                System.out.print("Sartu kotxearen motorea: ");
-                kotxea1.setMotorea(sarrera.next());
             
                 writer.write(kotxea1.idatzi());
                 writer.newLine();
@@ -76,7 +46,10 @@ public class Kotxea_Aldatu
         }
         else if(aukera.equals("n"))
         {
-            
+
+        }
+        else{
+            System.out.println("Aukera ez da zuzena. Sartu berriro:");
         }
 
         sarrera.close();
