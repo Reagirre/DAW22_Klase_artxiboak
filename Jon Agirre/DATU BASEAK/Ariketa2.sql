77532327125dd5b8205913b83a874dda3be5af8e
Reagirre <118358411+Reagirre@users.noreply.github.com>
2023-01-13 11:51:23 +0100

    Cambio

---
+++ Jon Agirre/DATU BASEAK/Ariketa2.sql
@@ -0,0 +1,23 @@
+insert into departa values (10, 'Kontaduria', 'Donosti');
+insert into departa values (20, 'Ikerketa', 'Bilbo');
+insert into departa values (30, 'Salmentak', 'Gazteiz');
+insert into departa values (40, 'Ekoizpena', 'Donosti');
+insert into departa values (50, 'Informatika', 'Bilbo');
+
+
+insert into Hornitzaile values(500, 'Jone','71111111','946612123','Artekale 12','Durango');
+
+insert into Hornitzaile values(501, 'Koldo','71211211','943121888','Isasi 29','Eibar');
+
+insert into Hornitzaile values(502, 'Mireia','74258258','946616000','Artibai 89','Markina-Xemein');
+
+insert into Hornitzaile values(503, 'Joseba','47258369','946020252','Goienkale 15','Durango');
+
+insert into Hornitzaile values(504, 'Ana','55666777','944343343','Korreo 22','Bilbo');
+
+insert into Hornitzaile values(505, 'Mikel', '25369852','603258145','Erribera 102','Bilbo');
+
+insert into Hornitzaile values(506, 'Arantza', '48888888','666777888','Barrenkale 22','Durango');
+
+
+
