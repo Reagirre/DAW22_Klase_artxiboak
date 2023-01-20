25a18bfb9c93f6c95378ca9c309e906bc46705f1
Reagirre <118358411+Reagirre@users.noreply.github.com>
2023-01-20 12:56:28 +0100

    Cambio

--- Jon Agirre/DATU BASEAK/Ariketa2.sql
+++ Jon Agirre/DATU BASEAK/Ariketa2.sql
@@ -1,23 +1,129 @@
-insert into departa values (10, 'Kontaduria', 'Donosti');
-insert into departa values (20, 'Ikerketa', 'Bilbo');
-insert into departa values (30, 'Salmentak', 'Gazteiz');
-insert into departa values (40, 'Ekoizpena', 'Donosti');
-insert into departa values (50, 'Informatika', 'Bilbo');
+SELECT 
+    *
+FROM
+    langile,
+    departa
+WHERE
+    langile.dept_zk = departa.dept_zk;
 
 
-insert into Hornitzaile values(500, 'Jone','71111111','946612123','Artekale 12','Durango');
 
-insert into Hornitzaile values(501, 'Koldo','71211211','943121888','Isasi 29','Eibar');
+SELECT 
+    *
+FROM
+    langile
+        LEFT JOIN
+    departa ON langile.dept_zk = departa.dept_zk;
 
-insert into Hornitzaile values(502, 'Mireia','74258258','946616000','Artibai 89','Markina-Xemein');
 
-insert into Hornitzaile values(503, 'Joseba','47258369','946020252','Goienkale 15','Durango');
 
-insert into Hornitzaile values(504, 'Ana','55666777','944343343','Korreo 22','Bilbo');
+SELECT 
+    *
+FROM
+    (SELECT 
+        COUNT(langile.lan_zk) zenbat, departa.depizen, departa.dept_zk
+    FROM
+        langile
+    INNER JOIN departa ON langile.dept_zk = departa.dept_zk
+    GROUP BY departa.dept_zk) AS taula
+WHERE
+    taula.zenbat > 3;
 
-insert into Hornitzaile values(505, 'Mikel', '25369852','603258145','Erribera 102','Bilbo');
 
-insert into Hornitzaile values(506, 'Arantza', '48888888','666777888','Barrenkale 22','Durango');
 
+
+SELECT 
+    departa.dept_zk,
+    departa.depizen,
+    COUNT(langile.lan_zk),
+    SUM(langile.soldata),
+    AVG(langile.soldata)
+FROM
+    langile
+        INNER JOIN
+    departa ON langile.dept_zk = departa.dept_zk
+GROUP BY departa.dept_zk
+having SUM(langile.soldata) > 6000 and SUM(langile.soldata) < 9000;
+
+
+
+SELECT 
+    departa.dept_zk,
+    departa.depizen,
+    COUNT(langile.lan_zk),
+    SUM(langile.soldata),
+    AVG(langile.soldata)
+FROM
+    langile
+        INNER JOIN
+    departa ON langile.dept_zk = departa.dept_zk
+GROUP BY departa.dept_zk
+HAVING AVG(langile.soldata) < 2000;
+
+
+
+SELECT 
+    departa.dept_zk,
+    departa.depizen,
+    COUNT(langile.lan_zk),
+    SUM(langile.soldata)
+FROM
+    langile
+        INNER JOIN
+    departa ON langile.dept_zk = departa.dept_zk
+WHERE
+    langile.lanbidea = 'langile'
+        OR langile.abizena LIKE '%e%'
+GROUP BY departa.dept_zk;
+
+SELECT 
+    langile.*
+FROM
+    langile
+WHERE
+    langile.soldata > (SELECT 
+            AVG(langile.soldata)
+        FROM
+            langile);
+            
+            
+SELECT 
+    langile.*
+FROM
+    langile
+WHERE
+    lanbidea = (SELECT 
+            lanbidea
+        FROM
+            langile
+        WHERE
+            abizena = 'Murua');
+
+
+SELECT 
+    langile.abizena, langile.lanbidea
+FROM
+    langile
+WHERE
+    lanbidea IN (SELECT 
+            lanbidea
+        FROM
+            langile
+        WHERE
+            dept_zk = 20);
+
+
+
+SELECT 
+    MAX(zenbat), taula.depizen, taula.dept_zk
+FROM
+    (SELECT 
+        COUNT(langile.lan_zk) zenbat,
+            departa.depizen,
+            departa.dept_zk
+    FROM
+        departa
+    INNER JOIN langile ON departa.dept_zk = langile.dept_zk
+    GROUP BY departa.dept_zk) AS taula;
 
 
