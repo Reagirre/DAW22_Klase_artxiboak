875c0d55348af8a65fd17e71c1b9926832086e47
Reagirre <118358411+Reagirre@users.noreply.github.com>
2023-02-27 09:58:06 +0100

    Cambio

--- Jon Agirre/PROGRAMAZIOA/Kapituloa9/Ariketa7/Kaixo.java
+++ Jon Agirre/PROGRAMAZIOA/Kapituloa9/Ariketa7/Kaixo.java
@@ -5,27 +5,8 @@ import javax.swing.JLabel;
 public class Kaixo {
     public static void main(String[] args) {
         
-        JFrame leihoa;
-        JPanel panela;
-        JLabel labela;
+        Leihoa l = new Leihoa();
 
-        leihoa = new JFrame();
-
-        leihoa.setBounds(0,0,300,100);
-
-        leihoa.setTitle("Kaixo Mundua!");
-
-        leihoa.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-
-        labela = new JLabel("Kaixo Mundua!");
-
-
-        panela = new JPanel();
-
-        panela.add(labela);
-
-        leihoa.getContentPane().add(panela);
-
-        leihoa.setVisible(true);
+        l.setVisible(true);
     }
 }
