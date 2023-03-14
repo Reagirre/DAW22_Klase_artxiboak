af3dc46fbf9183dc98f11749f2104898064ff0be
Reagirre <118358411+Reagirre@users.noreply.github.com>
2023-03-14 10:05:45 +0100

    Cambio

--- Jon Agirre/GARAPEN INGURUNEAK/Kapituloa4/Probak/src/konoa/Konoa.java
+++ Jon Agirre/GARAPEN INGURUNEAK/Kapituloa4/Probak/src/konoa/Konoa.java
@@ -3,19 +3,19 @@ import java.util.*;
 public class Konoa {
     public static void main(String[] args) throws Exception {
         Scanner sarrera = new Scanner(System.in);
-        double radio;
+        double erradioa;
         double height;
         double area;
         double volum;
 
         System.out.print("Sartu konoaren erradioa: ");
-        radio = sarrera.nextDouble();
+        erradioa = sarrera.nextDouble();
 
         System.out.print("Sartu konoaren altuera: ");
         height = sarrera.nextDouble();
 
-        area = Math.PI * radio * radio + Math.PI * radio * Math.sqrt(height*height+radio*radio);
-        volum = Math.PI * radio * radio * height / 3;
+        area = Math.PI * erradioa * erradioa + Math.PI * erradioa * Math.sqrt(height*height+erradioa*erradioa);
+        volum = Math.PI * erradioa * erradioa * height / 3;
 
         System.out.printf("Azalera = %.2f\n", area);
         System.out.printf("Bolumena = %.2f\n", volum);
